b4c9657897aa8681426e5efc0b6e44bc6524e431
Mitchell Hession <131897547+mitchellhession@users.noreply.github.com>
2023-07-02 18:33:50 -0400

    Update na_macbook_qsales.sql

--- na_macbook_qsales.sql
+++ na_macbook_qsales.sql
@@ -6,13 +6,14 @@ SELECT
   ROUND(SUM(orders.usd_price),2) AS total_sales,
   ROUND(AVG(orders.usd_price),2) AS aov
 FROM elist.orders AS orders
+-- Joined to customers and geo_lookup tables.
 LEFT JOIN elist.customers AS customers
   ON orders.customer_id=customers.id
 LEFT JOIN elist.geo_lookup AS geo_lookup
   ON customers.country_code=geo_lookup.country
---Filtered for only North America and Macbooks
+--Filtered for only North America and Macbooks.
 WHERE LOWER(orders.product_name) LIKE '%macbook%'
 AND region = 'NA'
--- Grouped by quarter and region. Organized by quarter and region
+-- Grouped by quarter and region. Organized by quarter and region.
 GROUP BY 1,2
 ORDER BY 1 DESC, 2;
